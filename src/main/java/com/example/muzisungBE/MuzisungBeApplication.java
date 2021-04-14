988b4a26bb4c6e2c9dcb652cb5d2c66fb6a97807
wooseokAn <aws7902@gmail.com>
2021-04-14 18:14:19 +0900

    Initial commit

---
+++ src/main/java/com/example/muzisungBE/MuzisungBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.muzisungBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzisungBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuzisungBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/muzisungBE/MuzisungBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muzisungBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzisungBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
